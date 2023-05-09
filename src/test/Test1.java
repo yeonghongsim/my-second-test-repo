91a3d7cc2260d1f58f95969aa8c6ec81b1ff572c
yeonghongsim <a93200551@gmail.com>
2023-05-09 14:11:50 +0900

    프로젝트 실행1

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -2,6 +2,7 @@ package test;
 
 public class Test1 {
 	public static void main(String[] ar) {
-		System.out.println("test");
+		System.out.println("프로젝트 시작");
+		System.out.println("프로젝트 종료");
 	}
 }
